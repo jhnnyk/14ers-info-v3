9e37297172fd0b28e47d5d0ce1dd900ba9282aa9
John Knotts <jhnnyk@gmail.com>
2020-12-22 19:01:15 -0700

    connect store

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,11 @@
   <div id="app">
     <TitleBar />
     <main>
+      <ul>
+        <li v-for="mtn in mtns" :key="mtn.mtn">
+          {{ mtn.mtn }}
+        </li>
+      </ul>
       <Nuxt />
     </main>
   </div>
@@ -13,6 +18,12 @@ import TitleBar from '@/components/TitleBar'
 export default {
   components: {
     TitleBar,
+  },
+
+  computed: {
+    mtns() {
+      return this.$store.state.store.mtns
+    },
   },
 }
 </script>
---
+++ store/store.js
@@ -0,0 +1,3 @@
+export const state = () => ({
+  mtns: [{ mtn: 'hello' }, { mtn: 'there' }],
+})
