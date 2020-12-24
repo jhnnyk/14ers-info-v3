f7828732f8243df5100f9f31c8343c8863c359c5
John Knotts <jhnnyk@gmail.com>
2020-12-24 14:33:43 -0700

    display mtnsByRange

--- components/MtnList.vue
+++ components/MtnList.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="mtn-list">
     <ul>
-      <li v-for="mtn in mtns" :key="mtn.slug">
-        {{ mtn.name }}
+      <li v-for="(range, index) in mtnsByRange" :key="index">
+        <h3>{{ range.name }}</h3>
+        <ul>
+          <li v-for="mtn in range.mtns" :key="mtn.slug">
+            <nuxt-link :to="{ name: 'index', params: { ...mtn } }">
+              {{ mtn.name }}
+            </nuxt-link>
+          </li>
+        </ul>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   computed: {
-    mtns() {
-      return this.$store.state.mtns.all
-    },
+    ...mapGetters('mtns', ['mtnsByRange']),
   },
 }
 </script>
--- store/mtns.js
+++ store/mtns.js
@@ -4,6 +4,24 @@ export const state = () => ({
   all: [],
 })
 
+export const getters = {
+  mtnsByRange: (state) => {
+    const ranges = []
+
+    state.all.forEach((mtn) => {
+      const index = ranges.findIndex((r) => r.name === mtn.range)
+
+      if (index === -1) {
+        ranges.push({ name: mtn.range, mtns: [mtn] })
+      } else {
+        ranges[index].mtns.push(mtn)
+      }
+    })
+
+    return ranges
+  },
+}
+
 export const mutations = {
   setMtns: (state, mtns) => {
     state.all = mtns
